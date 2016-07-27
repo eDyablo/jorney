2ecc974b6948822f4a978f419f9679312fc43646
Ed Yablonsky <eyablonsky@gmail.com>
2016-07-27 16:35:55 +0300

    Hello World using ZeroMQ.

---
+++ jorney/zmq/hw/server/main/src/ServerMain.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+#include "zmq.hpp"
+
+int main() {
+	zmq::context_t context(1);
+	zmq::socket_t socket(context, ZMQ_REP);
+  socket.bind("tcp://*:5555");
+	while (true) {
+		zmq::message_t request;
+		socket.recv(&request);
+		std::cout << "Received Hello" << std::endl;
+		::Sleep(1);
+		zmq::message_t reply (5);
+		::memcpy(reply.data (), "World", 5);
+		socket.send(reply);
+	}
+	return 0;
+}
