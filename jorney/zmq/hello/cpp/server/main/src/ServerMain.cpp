c010dc3547b1778833417ec804dd152897de5cfc
Ed Yablonsky <eyablonsky@gmail.com>
2016-08-04 10:40:43 +0300

    Refactored C++ version of 0MQ hello example.

--- jorney/zmq/hello/cpp/server/main/src/ServerMain.cpp
+++ jorney/zmq/hello/cpp/server/main/src/ServerMain.cpp
@@ -1,22 +1,62 @@
 #include <string>
 #include <iostream>
-
 #include "zmq.hpp"
 #include "basics/Thread.h"
 
+namespace zmqhello {
+	class Server {
+	public:
+		Server() :
+			context(1),
+			channel(context, ZMQ_REP) {
+		}
+
+		void run() {
+			initialize();
+			communicate();
+		}
+
+	private:
+		void initialize() {
+			channel.bind("tcp://*:5555");
+		}
+
+		void communicate() {
+			while (true) {
+				processOneRequest();
+				busy();
+			}
+		}
+
+		void processOneRequest() {
+			std::cout << "Received " << receive() << std::endl;
+			replay("World");
+		}
+
+		std::string receive() {
+			zmq::message_t request;
+			channel.recv(&request);
+			return std::string((char*)request.data(), request.size());
+		}
+
+		void replay(std::string const& text) {
+			zmq::message_t reply(text.size());
+			::memcpy(reply.data(), text.c_str(), text.size());
+			channel.send(reply);
+		}
+
+		void busy() {
+			basics::sleep(1);
+		}
+
+	private:
+		zmq::context_t context;
+		zmq::socket_t channel;
+	};
+}
+
 int main() {
-	zmq::context_t context(1);
-	zmq::socket_t socket(context, ZMQ_REP);
-	socket.bind("tcp://*:5555");
-	while (true) {
-		zmq::message_t request;
-		socket.recv(&request);
-		std::cout << "Received Hello" << std::endl;
-		basics::sleep(1);
-		zmq::message_t reply (5);
-		::memcpy(reply.data (), "World", 5);
-		socket.send(reply);
-		basics::sleep(1);
-	}
+	zmqhello::Server server;
+	server.run();
 	return 0;
 }
