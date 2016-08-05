da36e5af7c342c2ffaced34f2105ebcd09180dbe
Ed Yablonsky <eyablonsky@gmail.com>
2016-08-05 15:47:20 +0300

    C++ broker has frontend and backend command line arguments

--- jorney/zmq/broker/cpp/simple/main/src/BrokerMain.cpp
+++ jorney/zmq/broker/cpp/simple/main/src/BrokerMain.cpp
@@ -1,7 +1,19 @@
 #include "Broker.hpp"
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
 
-int main() {
-	zmqbroker::SimpleBroker broker(5559, 5560);
+int main(int argc, char* argv[]) {
+	if (argc < 3) {
+		std::cout << "Specify frontend and backend ports\n";
+		return 1;
+	}
+	std::stringstream params;
+	std::copy(&argv[1], &argv[argc], std::ostream_iterator<char*>(params, " "));
+	unsigned int frontend, backend;
+	params >> frontend >> backend;
+	zmqbroker::SimpleBroker broker(frontend, backend);
 	broker.run();
 	return 0;
 }
