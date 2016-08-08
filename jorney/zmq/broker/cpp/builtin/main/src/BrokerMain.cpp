4d61745a64e456a93ca86058da00ebc89cdf4aa1
Ed Yablonsky <eyablonsky@gmail.com>
2016-08-08 11:46:27 +0300

    C++ version of 0MQ request-reply broker that uses built-in proxy.

---
+++ jorney/zmq/broker/cpp/builtin/main/src/BrokerMain.cpp
@@ -0,0 +1,17 @@
+#include "Broker.hpp"
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+
+int main(int argc, char* argv[]) {
+	unsigned int frontend = 5559, backend = 5560;
+	if (argc > 2) {
+		std::stringstream params;
+		std::copy(&argv[1], &argv[argc], std::ostream_iterator<char*>(params, " "));
+		params >> frontend >> backend;
+	}	
+	zmqbroker::BuiltinBroker broker(frontend, backend);
+	broker.run();
+	return 0;
+}
