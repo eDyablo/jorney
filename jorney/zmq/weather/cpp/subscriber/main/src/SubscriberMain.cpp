c1e624e287a75e04e5fb9886e47fb219688b1a79
Ed Yablonsky <eyablonsky@gmail.com>
2016-08-11 16:33:20 +0300

    Refactored C++ version of 0MQ weather subscriber

--- jorney/zmq/weather/cpp/subscriber/main/src/SubscriberMain.cpp
+++ jorney/zmq/weather/cpp/subscriber/main/src/SubscriberMain.cpp
@@ -1,28 +1,25 @@
+#include <algorithm>
+#include <exception>
 #include <iostream>
-#include <sstream>
+#include <string>
+#include <vector>
 
 #include "zmq.hpp"
+#include "SubscriberProgramm.hpp"
 
-int main() {
-	zmq::context_t context(1);
-	std::cout << "Collecting updates from weather server..." << std::endl;
-	zmq::socket_t subscriber(context, ZMQ_SUB);
-	subscriber.connect("tcp://localhost:5556");
-	const char* filter = "10001 ";
-	subscriber.setsockopt(ZMQ_SUBSCRIBE, filter, strlen(filter));
-	int update_nbr;
-	long total_temp = 0;
-	for (update_nbr = 0; update_nbr < 100; update_nbr++) {
-		zmq::message_t update;
-		int zipcode, temperature, relhumidity;
-		subscriber.recv(&update);
-		std::istringstream iss(static_cast<char*>(update.data()));
-		iss >> zipcode >> temperature >> relhumidity ;
-		std::cout << "Got " << (int)(temperature / relhumidity) << "F\n";
-		total_temp += temperature;		
-	}
-	std::cout	<< "Average temperature for zipcode '" << filter
-			<< "' was " << (int)(total_temp / update_nbr) << "F"
-			<< std::endl;
-	return 0;
+int main(int argc, char* argv[]) {
+  try {
+    std::vector<std::string> args;
+    std::copy(&argv[1], &argv[argc], std::back_inserter(args));
+    zmqweather::SubscriberProgramm program;
+    program.run(args);
+    return 0;
+  }
+  catch (std::exception const& e) {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+  catch (...) {
+    std::cerr << "Something went wrong\n";
+  }
 }
