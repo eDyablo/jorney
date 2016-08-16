c3ecb55f87c6e7c7a468fd5eb2ab26858e4d0daf
Ed Yablonsky <eyablonsky@gmail.com>
2016-08-16 12:03:52 +0300

    Refactored C++ version of 0MQ hello client

---
+++ jorney/zmq/hello/cpp/client/main/src/Client.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "zmq.hpp"
+#include <iostream>
+#include <string>
+
+namespace zmqhello {
+  class Client {
+  public:
+    Client() :
+      context(1),
+      serviceChannel(context, ZMQ_REQ) {
+    }
+
+    void communicate(std::string& serviceEndpoint, size_t requestNumber) {
+      connect(serviceEndpoint);
+      communicate(requestNumber);
+    }
+
+  private:
+    void connect(std::string& serviceEndpoint) {
+      std::cout << "Connecting to hello server " << serviceEndpoint << " ...\n";
+      serviceChannel.connect("tcp://" + serviceEndpoint);
+    }
+
+    void communicate(size_t requestNumber) {
+      for (int i = 1; i <= requestNumber; ++i)
+        makeRequest("Hello");
+    }
+
+    void makeRequest(std::string const& text) {
+      std::cout << "Sending " << text << "..."
+          << " Received: " << request(text) << std::endl;
+    }
+
+    std::string request(std::string const& text) {
+      send(text);
+      return receive();
+    }
+
+    void send(std::string const& text) {
+      zmq::message_t request(text.size());
+      ::memcpy(request.data(), text.c_str(), text.size());
+      serviceChannel.send(request);
+    }
+
+    std::string receive() {
+      zmq::message_t reply;
+      serviceChannel.recv(&reply);
+      return std::string((char*)reply.data(), reply.size());
+    }
+
+  private:
+    zmq::context_t context;
+    zmq::socket_t serviceChannel;
+  };
+}
