14ba426c55381179d210024a35a7d480e1b1ce2c
Ed Yablonsky <eyablonsky@gmail.com>
2016-08-16 12:03:52 +0300

    Refactored C++ version of 0MQ hello client

--- jorney/zmq/hello/cpp/client/main/src/ClientMain.cpp
+++ jorney/zmq/hello/cpp/client/main/src/ClientMain.cpp
@@ -1,67 +1,25 @@
-#include <string>
+#include "ClientProgramm.hpp"
+#include <algorithm>
+#include <exception>
 #include <iostream>
-#include "zmq.hpp"
-
-namespace zmqhello {
-	class Client {
-	public:
-		Client(std::string const& anEndpoint) :
-			endpoint(anEndpoint),
-			context(1),
-			channel(context, ZMQ_REQ) {
-		}
-
-		void run() {
-			initialize();
-			communicate();
-		}
-
-	private:
-		void initialize() {
-			std::cout << "Connecting to hello server "<< endpoint << " ...\n";
-			channel.connect("tcp://" + endpoint);
-		}
-
-		void communicate() {
-			int const requestNumber = 10;
-			for (int i = 1; i <= requestNumber; ++i)
-				makeRequest("Hello");
-		}
-
-		void makeRequest(std::string const& text) {
-			std::cout << "Sending " << text << "..."
-					<< " Received: " << request(text) << std::endl;
-		}
-
-		std::string request(std::string const& text) {
-			send(text);
-			return receive();
-		}
-
-		void send(std::string const& text) {
-			zmq::message_t request(text.size());
-			::memcpy(request.data(), text.c_str(), text.size());
-			channel.send(request);
-		}
-
-		std::string receive() {
-			zmq::message_t reply;
-			channel.recv(&reply);
-			return std::string((char*)reply.data(), reply.size());
-		}
-
-	private:
-		std::string endpoint;
-		zmq::context_t context;
-		zmq::socket_t channel;
-	};
-}
+#include <iterator>
+#include <string>
+#include <vector>
 
 int main(int argc, char* argv[]) {
-	std::string endpoint = "localhost:5559";
-	if (argc > 1)
-		endpoint = argv[1];
-	zmqhello::Client client(endpoint);
-	client.run();
-	return 0;
+  try {
+    std::vector<std::string> args;
+    std::copy(&argv[1], &argv[argc], std::back_inserter(args));
+    zmqhello::ClientProgramm program;
+    program.run(args);
+    return 0;
+  }
+  catch (std::exception const& e) {
+    std::cerr << e.what() << ".\n";
+    return 1;
+  }
+  catch (...) {
+    std::cerr << "Something went wrong.\n";
+    return 2;
+  }
 }
