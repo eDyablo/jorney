99142576debb41d54f7f5d95617238bfe06682da
Ed Yablonsky <eyablonsky@gmail.com>
2016-08-18 12:41:05 +0300

    Refactored C++ version of 0MQ hello multi threaded service worker

--- jorney/zmq/hello/cpp/mtserver/main/src/Worker.hpp
+++ jorney/zmq/hello/cpp/mtserver/main/src/Worker.hpp
@@ -1,25 +1,61 @@
 #pragma once
 
 #include "zmq.hpp"
-
-#include <iostream>
-#include <thread>
 #include <chrono>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <algorithm>
 
 namespace zmqhello {
-	void worker(zmq::context_t* context) {
-		zmq::socket_t socket(*context, ZMQ_REP);
-		socket.connect("inproc://workers");
-		while (true) {
-			zmq::message_t request;
-			socket.recv(&request);
-			std::cout << std::this_thread::get_id() << " Received request: [" << (char*)request.data() << "]\n";
+  class Worker {
+  public:
+    Worker(zmq::context_t& theContext):
+      context(theContext),
+      channel(context, ZMQ_REP) {
+    }
 
-			std::this_thread::sleep_for(std::chrono::seconds(1));
+    void run() {
+      channel.connect("inproc://workers");
+      communicate();
+    }
 
-			zmq::message_t reply (6);
-			::memcpy((void*)reply.data(), "World", 6);
-			socket.send(reply);
-		}
-	}
+  private:
+    void communicate() {
+      while (true)
+        handle(receive());
+    }
+
+    std::string receive() {
+      zmq::message_t request;
+      channel.recv(&request);
+      return std::string(static_cast<char*>(request.data()), request.size());
+    }
+
+    void handle(std::string const& request) {
+      std::cout << std::this_thread::get_id() <<
+          " Received request: [" << request << "]\n";
+      busy();
+      answer("World");
+    }
+
+    void busy() {
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    void answer(std::string const& text) {
+      zmq::message_t reply(text.size());
+      std::copy(text.begin(), text.end(), static_cast<char*>(reply.data()));
+      channel.send(reply);
+    }
+
+  private:
+    zmq::context_t& context;
+    zmq::socket_t channel;
+  };
+
+  void worker(zmq::context_t* context) {
+    Worker worker(*context);
+    worker.run();
+  }
 }
