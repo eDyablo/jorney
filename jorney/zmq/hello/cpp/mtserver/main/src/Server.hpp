040bc8902581fbb071329955f483051e287131af
Ed Yablonsky <eyablonsky@gmail.com>
2016-08-18 16:48:13 +0300

    C++ version of 0MQ hello multi threaded service terminates workers and proxy

--- jorney/zmq/hello/cpp/mtserver/main/src/Server.hpp
+++ jorney/zmq/hello/cpp/mtserver/main/src/Server.hpp
@@ -1,63 +1,103 @@
 #pragma once
 
-#include "zmq.hpp"
 #include "Worker.hpp"
-
+#include "zmq.hpp"
+#include <algorithm>
 #include <string>
 #include <thread>
+#include <vector>
 
 namespace zmqhello {
-	class Server {
-	public:
-		Server() :
-			context(1),
-			clients(context, ZMQ_ROUTER),
-			workers(context, ZMQ_DEALER) {
-		}
+  class Server {
+  public:
+    Server() :
+      context(1),
+      clientChannel(context, ZMQ_ROUTER),
+      taskChannel(context, ZMQ_DEALER),
+      workerControlChannel(context, ZMQ_PUB),
+      proxyControlChannel(context, ZMQ_PUB) {
+    }
 
-		void communicate(std::string const& endPoint, size_t workersNumber) {
-			connect(endPoint);
-			startWorkers(workersNumber);
-			communicate();
-		}
+    void communicate(std::string const& endPoint, size_t workersNumber) {
+      connect(endPoint);
+      startWorkers(workersNumber);
+      communicate();
+    }
 
-	private:
-		void connect(std::string const& endPoint) {
-			connectClients(endPoint);
-			connectWorkers();
-		}
+    void stop() {
+      stopWorkers();
+      commandProxy("TERMINATE");
+    }
 
-		void connectClients(std::string const& endPoint) {
-			if (useSoloMode(endPoint))
-				clients.bind("tcp://" + endPoint);
-			else
-				clients.connect("tcp://" + endPoint);
-		}
+  private:
+    void connect(std::string const& endPoint) {
+      connectClientChannel(endPoint);
+      connectWorkerChannel();
+      connectProxyControlChannel();
+    }
 
-		bool useSoloMode(std::string const& endPoint) const {
-			return endPoint.find_first_of('*') == 0;
-		}
+    void connectClientChannel(std::string const& endPoint) {
+      if (useSoloMode(endPoint))
+        clientChannel.bind("tcp://" + endPoint);
+      else
+        clientChannel.connect("tcp://" + endPoint);
+    }
 
-		void connectWorkers() {
-			workers.bind ("inproc://workers");
-		}
+    bool useSoloMode(std::string const& endPoint) const {
+      return endPoint.find_first_of('*') == 0;
+    }
 
-		void startWorkers(size_t number) {
-			for (size_t i = 0; i < number; ++i)
-				startWorker();
-		}
+    void connectWorkerChannel() {
+      taskChannel.bind("inproc://task");
+      workerControlChannel.bind("inproc://worker");
+    }
 
-		void startWorker() {
-			std::thread* wt = new std::thread(worker, &context);
-		}
+    void connectProxyControlChannel() {
+      proxyControlChannel.bind("inproc://proxy");
+    }
 
-		void communicate() {
-			zmq::proxy(clients, workers, 0);
-		}
+    void startWorkers(size_t number) {
+      for (size_t i = 0; i < number; ++i)
+        startWorker();
+    }
 
-	private:
-		zmq::context_t context;
-		zmq::socket_t clients;
-		zmq::socket_t workers;
-	};
+    void startWorker() {
+      workers.push_back(std::thread(worker, &context));
+    }
+
+    void communicate() {
+      zmq::socket_t control(context, ZMQ_SUB);
+      control.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+      control.connect("inproc://proxy");
+      zmq::proxy_steerable(clientChannel, taskChannel, 0, control);
+    }
+
+    void commandWorkers(std::string const& command) {
+      send(workerControlChannel, command);
+    }
+
+    void commandProxy(std::string const& command) {
+      send(proxyControlChannel, command);
+    }
+
+    void send(zmq::socket_t& channel, std::string const& text) {
+      zmq::message_t message(text.size());
+      std::copy(text.begin(), text.end(), static_cast<char*>(message.data()));
+      channel.send(message);
+    }
+
+    void stopWorkers() {
+      commandWorkers("STOP");
+      for (auto i = workers.begin(); i != workers.end(); ++i)
+        (*i).join();
+    }
+
+  private:
+    zmq::context_t context;
+    zmq::socket_t clientChannel;
+    zmq::socket_t taskChannel;
+    zmq::socket_t workerControlChannel;
+    zmq::socket_t proxyControlChannel;
+    std::vector<std::thread> workers;
+  };
 }
--- jorney/zmq/hello/cpp/mtserver/main/src/Worker.hpp
+++ jorney/zmq/hello/cpp/mtserver/main/src/Worker.hpp
@@ -1,40 +1,89 @@
 #pragma once
 
+#include "shims/attribute/GetBegin.h"
+#include "shims/attribute/GetSize.h"
 #include "zmq.hpp"
+#include <algorithm>
 #include <chrono>
 #include <iostream>
 #include <string>
 #include <thread>
-#include <algorithm>
 
 namespace zmqhello {
   class Worker {
   public:
     Worker(zmq::context_t& theContext):
       context(theContext),
-      channel(context, ZMQ_REP) {
+      taskChannel(context, ZMQ_REP),
+      controlChannel(context, ZMQ_SUB),
+      active(false) {
+      polls[0] = {taskChannel, 0, ZMQ_POLLIN, 0};
+      polls[1] = {controlChannel, 0, ZMQ_POLLIN, 0};
     }
 
     void run() {
-      channel.connect("inproc://workers");
+      connectTaskChannel();
+      connectControlChannel();
+      activate();
       communicate();
     }
 
   private:
+    void connectTaskChannel() {
+      taskChannel.connect("inproc://task");
+    }
+
+    void connectControlChannel() {
+      controlChannel.connect("inproc://worker");
+      controlChannel.setsockopt(ZMQ_SUBSCRIBE, "", 0);
+    }
+
+    void activate() {
+      active = true;
+    }
+
     void communicate() {
-      while (true)
-        handle(receive());
+      while (isActive()) {
+        poll();
+        process();
+      }
     }
 
-    std::string receive() {
-      zmq::message_t request;
-      channel.recv(&request);
-      return std::string(static_cast<char*>(request.data()), request.size());
+    bool isActive() const {
+      return active;
     }
 
-    void handle(std::string const& request) {
+    void poll() {
+      using namespace shims;
+      zmq::poll(GetBegin(polls), GetSize(polls), -1);
+    }
+
+    void process() {
+      if (hasWorkload())
+        handleWorkload(getWorkload());
+      if (hasControlSignal())
+        handleControlSignal(getControlSignal());
+    }
+
+    bool hasWorkload() const {
+      return hasMessage(polls[0]);
+    }
+
+    bool hasControlSignal() const {
+      return hasMessage(polls[1]);
+    }
+
+    bool hasMessage(zmq::pollitem_t const& item) const {
+      return item.revents & ZMQ_POLLIN;
+    }
+
+    std::string getWorkload() {
+      return receive(taskChannel);
+    }
+
+    void handleWorkload(std::string const& request) {
       std::cout << std::this_thread::get_id() <<
-          " Received request: [" << request << "]\n";
+          " Received: [" << request << "]" << std::flush;
       busy();
       answer("World");
     }
@@ -46,12 +95,35 @@ namespace zmqhello {
     void answer(std::string const& text) {
       zmq::message_t reply(text.size());
       std::copy(text.begin(), text.end(), static_cast<char*>(reply.data()));
-      channel.send(reply);
+      taskChannel.send(reply);
+      std::cout << " Replied: [" << text << "]\n" << std::flush;
+    }
+
+    std::string getControlSignal() {
+      return receive(controlChannel);
+    }
+
+    void handleControlSignal(std::string const& signal) {
+      if (signal == "STOP")
+        stop();
+    }
+
+    std::string receive(zmq::socket_t& channel) {
+      zmq::message_t request;
+      channel.recv(&request);
+      return std::string(static_cast<char*>(request.data()), request.size());
+    }
+
+    void stop() {
+      active = false;
     }
 
   private:
     zmq::context_t& context;
-    zmq::socket_t channel;
+    zmq::socket_t taskChannel;
+    zmq::socket_t controlChannel;
+    zmq::pollitem_t polls[2];
+    bool active;
   };
 
   void worker(zmq::context_t* context) {
