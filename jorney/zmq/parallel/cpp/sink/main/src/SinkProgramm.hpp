1d7c5b87153886cb79c6cf167ed02d4f98459440
Ed Yablonsky <eyablonsky@gmail.com>
2016-08-18 12:04:23 +0300

    C++ version of 0MQ parallel task sink sends stop singnal to workers over a control channel

--- jorney/zmq/parallel/cpp/sink/main/src/SinkProgramm.hpp
+++ jorney/zmq/parallel/cpp/sink/main/src/SinkProgramm.hpp
@@ -8,15 +8,21 @@ namespace zmqparallel {
 	class SinkProgramm {
 	public:
 		void run(std::vector<std::string> const& args) {
-			sink.communicate(getSinkEndpoint(args));
+			this->args = args;
+			sink.communicate(getSinkEndpoint(), getWorkerControlEndpoint());
 		}
 
 	private:
-		std::string getSinkEndpoint(std::vector<std::string> const& args) {
+		std::string getSinkEndpoint() const {
 			return args.size() < 1 ? "*:5558" : args[0];
+		}
+
+		std::string getWorkerControlEndpoint() const {
+			return args.size() < 2 ? "*:5559" : args[1];
 		}
 
 	private:
 		Sink sink;
+		std::vector<std::string> args;
 	};
 }
--- jorney/zmq/parallel/cpp/worker/main/src/WorkerProgramm.hpp
+++ jorney/zmq/parallel/cpp/worker/main/src/WorkerProgramm.hpp
@@ -8,19 +8,26 @@ namespace zmqparallel {
   class WorkerProgramm {
   public:
   	void run(std::vector<std::string> const& args) {
-  		worker.communicate(getTaskEndpoint(args), getSinkEndpoint(args));
+      this->args = args;
+  		worker.communicate(getTaskEndpoint(), getSinkEndpoint(),
+          getControlEndpoint());
   	}
 
   private:
-  	std::string getTaskEndpoint(std::vector<std::string> const& args) const {
+  	std::string getTaskEndpoint() const {
   		return (args.size() < 1) ? "localhost:5557" : args[0];
   	}
 
-  	std::string getSinkEndpoint(std::vector<std::string> const& args) const {
+  	std::string getSinkEndpoint() const {
   		return (args.size() < 2) ? "localhost:5558" : args[1];
   	}
 
+    std::string getControlEndpoint() const {
+      return args.size() < 3 ? "localhost:5559" : args[2];
+    }
+
   private:
   	Worker worker;
+    std::vector<std::string> args;
   };
 }
